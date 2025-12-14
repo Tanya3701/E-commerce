2098ff75a246745d3e9fb0648b2b882c7a1e0705
Bogomazova Tanya <ip-bogomazova@mail.ru>
2025-12-14 13:57:08 +0500

    homework16.1/1

--- src/category.py
+++ src/category.py
@@ -25,8 +25,11 @@ class Category:
 
     def add_product(self, product):
         """Метод для добавления товаров в категорию"""
-        self.__products.append(product)
-        self.product_count += 1
+        if isinstance(product, Product):
+            self.__products.append(product)
+            self.product_count += 1
+        else:
+            raise TypeError
 
     @property
     def products(self):
